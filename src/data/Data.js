6633d69c6660b217b11220c4e9170c7e7f0c54ef
Alejandra Soto Arevalo <asoto89@gmail.com>
2019-06-14 23:25:42 +0200

    change fetch url

--- src/data/Data.js
+++ src/data/Data.js
@@ -1,4 +1,4 @@
-const ENDPOINT = 'https://pokeapi.co/api/v2/pokemon/?offset=20&limit=25';
+const ENDPOINT = 'https://pokeapi.co/api/v2/pokemon/?limit=25';
 
 const fetchData = () => fetch(ENDPOINT).then(response => response.json());
 
