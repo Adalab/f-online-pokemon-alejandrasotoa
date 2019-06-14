aa9ff41874a4173fec6373afa1760a7fb932f080
Alejandra Soto Arevalo <asoto89@gmail.com>
2019-06-14 19:30:15 +0200

    added local storage

--- src/App.js
+++ src/App.js
@@ -7,22 +7,44 @@ class App extends React.Component {
     this.state = {
       pokemons: []
     };
-    this.fetchPokemons();
   }
 
   fetchPokemons() {
     fetch("https://pokeapi.co/api/v2/pokemon/?offset=20&limit=25")
       .then(response => response.json())
-      .then(data => data.results.map(item => {
-        let newArray;
-        fetch(item.url)
-          .then(response => response.json())
-          .then(data => { this.setState(prevState => ({ pokemons: [...prevState.pokemons, { ...data }] })) })
-      }))
+      .then(data => {
+        data.results.map(item =>
+          fetch(item.url)
+            .then(response => response.json())
+            .then(data => this.setState(prevState => ({ pokemons: [...prevState.pokemons, { ...data }] }))
+            )
+        )
+      }
+      )
+  }
+
+  componentDidMount() {
+    const isLocalStorage = JSON.parse(localStorage.getItem('pokemonList'));
+    if (isLocalStorage.length === 0) {
+      this.fetchPokemons();
+    } else {
+      this.setState({ pokemons: [ ...isLocalStorage ] })
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.state.pokemons.length === 25) {
+      localStorage.setItem('pokemonList', JSON.stringify(this.state.pokemons));
+    }
   }
 
   render() {
-    return <div className="App"> Hola </div>;
+    if (this.state.pokemons.length === 0) {
+      return <p>Cargando...</p>
+    }
+    return <div className="App">
+      {/* {this.state.pokemons.map(item => <li>{item.name}</li>)} */}
+    </div>;
   }
 }
 
